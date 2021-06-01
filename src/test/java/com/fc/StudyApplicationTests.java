aeaa6be0fbb21766cd690ebea68699fb2dad2f34
fc <910730674@qq.com>
2021-06-01 10:21:54 +0800

    初始化项目成功

---
+++ src/test/java/com/fc/StudyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+//import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
